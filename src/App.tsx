0b1a8c4e6f85399a2fba8c5a363605b5833cd04e
maintainline <z.seon.dev@gmail.com>
2025-08-11 10:59:44 +0900

    [docs]todo제작해 보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,47 @@
 import { useState } from 'react';
 import TodoList from './components/todos/TodoList';
 import TodoWrite from './components/todos/TodoWrite';
-import { ITodoType, TodoType } from './types/todoType';
 
+// 공통으로 사용하는 type 정의 및 interface 는 별도의 폴더에 보관하자.
+import { ITodoType, TodoType } from './types/todoType';
+import TodoItem from './components/todos/TodoItem';
+
+// 테스트를 위한 목업 데이터(/src/api/dummy.ts 추천)
+const initialTodos: TodoType[] = [
+  { id: 'a', title: '제목 1입니다.', completed: false },
+  { id: 'b', title: '제목 2입니다.', completed: true },
+  { id: 'c', title: '제목 3입니다.', completed: false },
+  { id: 'd', title: '제목 4입니다.', completed: true },
+  { id: 'e', title: '제목 5입니다.', completed: false },
+];
 
 function App(): JSX.Element {
   //ts
   //{id:"", title:"",completed:false}
-  const [todos, setTodos] = useState<(ITodoType | TodoType)[]>([]);
+  const [todos, setTodos] = useState<(ITodoType | TodoType)[]>(initialTodos);
 
   // todos를 업데이트 하는 함수
   const handleTodoUpdate = (): void => {
     //setTodos()
   };
   //todo 목록에서 실행할 함수들
-  const onToggle = (): void => {};
-  const onDelete = (): void => {};
-  const onEdit = (): void => {};
+  const onToggle = (id: string): void => {
+    console.log('onToggle:', id);
+    // 전달받은 ID 를 이용해서 .map으로 id가 같으면 찾아서 completed 변경
+    const arr: TodoType[] = todos.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+    );
+    setTodos(arr);
+  };
+  const onDelete = (id: string): void => {
+    console.log('onDelete:', id);
+    // 전달받은 ID 를 제외한 나머지만 모아서 목록 변경
+    const arr: TodoType[] = todos.filter(todo => todo.id !== id);
+    setTodos(arr);
+  };
+  const onEdit = (): void => {
+    console.log('onEdit');
+  };
   //tsx
   return (
     <div>
--- src/components/todos/TodoList.tsx
+++ src/components/todos/TodoList.tsx
@@ -4,8 +4,8 @@ import TodoItem from './TodoItem';
 //컴포넌트 + Props
 type TodoListProps = {
   todos: TodoType[];
-  onToggle: () => void;
-  onDelete: () => void;
+  onToggle: (id: string) => void;
+  onDelete: (id: string) => void;
   onEdit: () => void;
 };
 
@@ -13,7 +13,22 @@ const TodoList = ({ todos, onToggle, onDelete, onEdit }: TodoListProps): JSX.Ele
   return (
     <div>
       <h2>할일목록</h2>
-      <TodoItem onToggle={onToggle} onDelete={onDelete} onEdit={onEdit} />
+      {/* 할일 즉, todos 는 여러개의 item 으로 구성된 배열이다. .map 으로 출력 */}
+      {todos.length === 0 ? (
+        <p>목록이 없습니다.</p>
+      ) : (
+        <ul>
+          {todos.map(item => (
+            <TodoItem
+              key={item.id}
+              todo={item}
+              onToggle={onToggle}
+              onDelete={onDelete}
+              onEdit={onEdit}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
